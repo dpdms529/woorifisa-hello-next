7fc73ed8aa9b6340ad708297ef9c53ccdc1f77e1
YE-EUN <musiclove529@naver.com>
2024-07-29 14:07:19 +0900

    not found test

--- app/products/[productId]/page.jsx
+++ app/products/[productId]/page.jsx
@@ -1,3 +1,8 @@
+import { notFound } from "next/navigation";
+
 export default function ProductDetail({ params }) {
+  if (params.productId > 20) {
+    notFound();
+  }
   return <div>Product {params.productId} details</div>;
 }
